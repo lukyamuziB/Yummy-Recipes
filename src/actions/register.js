ad4db31527f382a720851b201cbba30bdd6661d9
lukyamuziB <lukyamuzibenon@gmail.com>
2018-03-10 19:24:02 +0300

    delete no longer needed files+dependencies

--- src/actions/register.js
+++ src/actions/register.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
+import toastr from 'toastr';
 
-const ROOT_URL = 'http://127.0.0.1:5000/api';
-
-
-export const REGISTER_USER = 'register_user';
-export const CREATE_CATEGORIES = 'create_categories';
-
+import {ROOT_URL} from '../index';
+import * as types from './actionTypes';
 
 export default  function registerUser(values, callback){
     const request = axios.post(`${ROOT_URL}/auth/register`, values)
-    .then(() => callback());
+    .then(() => callback())
+    .catch(xhr=>{
+        console.log(xhr.response.data.Error)
+        toastr.success(xhr.response.data.Error)
+
+    });
 
     return {
-        type: REGISTER_USER,
+        type: types.REGISTER_USER,
         payload: request
     };
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,4 +24,3 @@ class Navbar extends Component {
 }
 
 export default Navbar;
-
