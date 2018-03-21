af72bedf3b0374df86d5c376e6058e6ad3f6a54c
lukyamuziB <lukyamuzibenon@gmail.com>
2018-03-21 04:46:44 +0300

    add tests for navbar component+ delete recipes and categories components

--- src/components/deleteResource.js
+++ src/components/deleteResource.js
@@ -1,32 +1,31 @@
-import React, {Component} from 'react';
-import {Link, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 import deleteCategory from '../actions/deleteCategoryAction';
 
-class DeleteCategory extends Component{
-
+class DeleteCategory extends Component {
 deleteCategories = (id) => {
-    this.props.deleteCategory(this.props.id).then(()=>
-        this.props.history.push("/dashboard"))
-        // window.location.reload()
+  this.props.deleteCategory(this.props.id).then(() =>
+    this.props.history.push('/dashboard'));
+  // window.location.reload()
 }
 
-    render(){
-        return(
-            <div>
-             <div id={`modal2${this.props.id}`} class="modal">
-              <div class="modal-content">
-              {this.props.id}
-                <h5> Are you sure want to delete this Category? </h5>
-                <button onClick = {this.deleteCategories}> Delete </button>
-                <Link to="/dashboard"> <button> Cancel </button> </Link>
-              </div>
-            </div>
-          </div>
-        );
-    }
+render() {
+  return (
+    <div>
+      <div id={`modal2${this.props.id}`} className="modal">
+        <div className="modal-content">
+          {this.props.id}
+          <h5> Are you sure want to delete this Category? </h5>
+          <button onClick={this.deleteCategories}> Delete </button>
+          <Link to="/dashboard"> <button> Cancel </button> </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 }
 
-export default connect(null, {deleteCategory})(withRouter(DeleteCategory));
+export default connect(null, { deleteCategory })(withRouter(DeleteCategory));
